99f4380bdb3800eb7506057a7b173205748ada00
nOthers <qimuyangtao@gmail.com>
2020-02-16 21:06:57 +0800

    重新封装pc端objnet接口，适应多连接方案切换

--- SubProjects/objnet/__init__.py
+++ SubProjects/objnet/__init__.py
@@ -1,14 +1,86 @@
 #!/usr/local/bin/python3
+import re
+import os
 import json
 import numpy
 import socket
 import itertools
 import threading
 numpy.seterr(all='ignore')
-LOCK = threading.RLock()
-SESSIONS = {}
+UNIQUE = itertools.count(1)
+EVENTS = {}
 
 
+class RemoteException(BaseException):
+    pass
+
+
+class INetwork:
+    # 访问对象网络服务
+    def request(self, pkgname, object, method, *params, timeout=None):
+        xid = next(UNIQUE) % 2**16
+        event = threading.Event()
+        EVENTS[xid] = event
+        self.send(pkgname, {
+            'xid': xid,
+            'object': object,
+            'method': method,
+            'params': params,
+        })
+        if not event.wait(timeout=timeout):
+            raise TimeoutError('remote not responsed')
+        if event.e is not None:
+            raise RemoteException(event.e)
+        return event.r
+
+    # 发送数据
+    def send(self, pkgname, data):
+        raise NotImplementedError
+
+    # 数据回调
+    def recv(self, data):
+        event = EVENTS.pop(data['xid'], None)
+        if event is not None:
+            event.r = data['r']
+            event.e = data['e']
+            event.set()
+
+
+# 获取默认设备号
+def get_default_serial():
+    serial = None
+    text = os.popen('adb devices').read()
+    for line in text.split('\n'):
+        if line.endswith('device'):
+            serial = line.split()[0]
+            break
+    return serial
+
+
+# 包名映射广播号
+def get_action_by_pkgname(pkgname):
+    return pkgname + '/objnet'
+
+
+class BroadcastNetwork(INetwork):
+    def __init__(self, serial=None):
+        if serial is None:
+            serial = get_default_serial()
+            assert serial is not None, 'device not found'
+        self.serial = serial
+
+    def send(self, pkgname, data):
+        action = get_action_by_pkgname(pkgname)
+        data = json.dumps(data).encode().hex()
+        text = os.popen(f'adb -s {self.serial} shell am broadcast -a {action} -e data {data}').read()
+        m = re.search(r', data="([0-9a-fA-F]*)"', text)
+        if not m:
+            raise EOFError('remote not responsed')
+        data = json.loads(bytes.fromhex(m.group(1)).decode())
+        self.recv(data)
+
+
+# 哈希运算
 def hashCode(string):
     h = numpy.int32(0)
     for char in string:
@@ -16,84 +88,35 @@ def hashCode(string):
     return int(h)
 
 
-# 应用名映射端口号
-def get_port_by_name(name):
-    return 2**15 + (hashCode(name) % 2**15)
+# 包名映射端口号
+def get_port_by_pkgname(pkgname):
+    return 2**15 + (hashCode(pkgname) % 2**15)
 
 
-# 访问远程设备反射服务
-def request(host, name_or_port, object, method, *params):
-    port = get_port_by_name(name_or_port) if isinstance(name_or_port, str) else name_or_port
-    addr = f'{host}:{port}'
-    with LOCK:
-        if addr not in SESSIONS:
-            SESSIONS[addr] = Session(host, port)
-        session = SESSIONS[addr]
-    return session.request(object, method, *params)
+class SocketNetwork(INetwork):
+    def __init__(self, host):
+        self.host = host
 
-
-class RemoteException(BaseException):
-    pass
-
-
-class Session:
-    def __init__(self, host, port):
+    def send(self, pkgname, data):
+        port = get_port_by_pkgname(pkgname)
+        sock = socket.create_connection((self.host, port,))
         try:
-            self.socket = socket.create_connection((host, port,))
-        except BaseException:
-            raise RemoteException('remote deny connection')
-        self.closed = False
-        self.buffer = b''
-        self.unique = itertools.count(1)
-        self.events = {}
-        threading.Thread(target=self.run, daemon=True).start()
-
-    # 请求反射服务
-    def request(self, object, method, *params):
-        xid = next(self.unique) % 2**16
-        event = threading.Event()
-        self.events[xid] = event
-        self.socket.send(json.dumps({
-            'xid': xid,
-            'object': object,
-            'method': method,
-            'params': params,
-        }).encode())
-        self.socket.send(b'\x00')
-        event.wait()
-        if event.e is not None:
-            raise RemoteException(event.e)
-        return event.r
-
-    # 回调反射服务
-    def response(self, data):
-        pack = json.loads(data.decode())
-        event = self.events.pop(pack['xid'], None)
-        if event is not None:
-            event.r = pack['r']
-            event.e = pack['e']
-            event.set()
-
-    # 线程轮询消息
-    def run(self):
-        while True:
-            try:
-                b = self.socket.recv(1024)
-            except socket.timeout:
-                continue
-            except ConnectionError:
-                b = b''
-            if not b:
-                break
-            self.buffer += b
-            split = self.buffer.split(b'\x00')
-            self.buffer = split.pop()
-            for s in split:
-                self.response(s)
-        self.closed = True
-
-    # 关闭连接
-    def close(self):
-        self.socket.close()
-        while not self.closed:
-            pass
+            sock.sendall(json.dumps(data).encode())
+            sock.sendall(b'\x00')
+            buffer = b''
+            while True:
+                try:
+                    b = sock.recv(1024)
+                except socket.timeout:
+                    continue
+                except ConnectionError:
+                    b = b''
+                if not b:
+                    raise EOFError('remote not responsed')
+                buffer += b
+                if b'\x00' in buffer:
+                    buffer = buffer.split(b'\x00')[0]
+                    self.recv(json.loads(buffer.decode()))
+                    break
+        finally:
+            sock.close()
